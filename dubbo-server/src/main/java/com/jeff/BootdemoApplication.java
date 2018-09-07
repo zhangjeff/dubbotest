7538a418726db0944adec51cc7b352ef2b17b146
youpeng.zhang <youpeng.zhang@chinaredstar.com>
2018-09-07 15:26:34 +0800

    commit

--- dubbo-server/src/main/java/com/jeff/BootdemoApplication.java
+++ dubbo-server/src/main/java/com/jeff/BootdemoApplication.java
@@ -1,12 +1,16 @@
 package com.jeff;
 
+import com.alibaba.dubbo.container.Main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class BootdemoApplication {
+public class BootdemoApplication{
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootdemoApplication.class, args);
+		Main.main(args);
+//    	SpringApplication.run(BootdemoApplication.class, args);
 	}
 }
